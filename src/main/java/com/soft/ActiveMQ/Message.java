398ecd972245cc2bb5053db53d245b8fd139f852
Mrshieh817 <18716398693@163.com>
2018-05-02 11:25:17 +0800

    完善logback_ActiveMq的自定义参数配置

--- src/main/java/com/soft/ActiveMQ/Message.java
+++ src/main/java/com/soft/ActiveMQ/Message.java
@@ -8,50 +8,49 @@ import org.apache.log4j.spi.LoggingEvent;
 import javax.jms.*;
 
 /**
-* @author 作者:大飞
-* @version 创建时间：2018年4月28日 下午3:16:04
-* 类说明
-*/
+ * @author 作者:大飞
+ * @version 创建时间：2018年4月28日 下午3:16:04 类说明
+ */
 
 public class Message implements MessageListener {
 
-    public Message() throws Exception {
-        // create consumer and listen queue
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.139.128:61616");
-        Connection connection = factory.createConnection();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        connection.start();
-        //注意这里JMSAppender只支持TopicDestination//
-        Destination topicDestination = session.createQueue("Jaycekon-MQ");
-        MessageConsumer consumer = session.createConsumer(topicDestination);
-        consumer.setMessageListener(this);
+	public Message() throws Exception {
+		// (测试用例)
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.139.128:61616");
+		Connection connection = factory.createConnection();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		connection.start();
+		// 注意这里JMSAppender只支持TopicDestination//
+		Destination topicDestination = session.createQueue("Jaycekon-MQ");
+		MessageConsumer consumer = session.createConsumer(topicDestination);
+		consumer.setMessageListener(this);
 
-        // log a message
-        Logger logger = Logger.getLogger(Message.class);
-        logger.info("Info Log.");
-        logger.warn("Warn Log");
-        logger.error("Error Log.");
+		// log a message
+		Logger logger = Logger.getLogger(Message.class);
+		logger.info("Info Log.");
+		logger.warn("Warn Log");
+		logger.error("Error Log.");
 
-        // clean up
-        Thread.sleep(1000);
-        consumer.close();
-        session.close();
-        connection.close();
-        System.exit(1);
-    }
+		// clean up
+		Thread.sleep(1000);
+		consumer.close();
+		session.close();
+		connection.close();
+		System.exit(1);
+	}
 
-    public void onMessage(javax.jms.Message message) {
-        try {
-            // receive log event in your consumer
-            LoggingEvent event = (LoggingEvent) ((ActiveMQObjectMessage) message).getObject();
-            System.out.println("Received log [" + event.getLevel() + "]: " + event.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public void onMessage(javax.jms.Message message) {
+		try {
+			// receive log event in your consumer
+			LoggingEvent event = (LoggingEvent) ((ActiveMQObjectMessage) message).getObject();
+			System.out.println("Received log [" + event.getLevel() + "]: " + event.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        new Message();
-    }
+	public static void main(String[] args) throws Exception {
+		new Message();
+	}
 
 }
--- src/main/java/com/soft/Logback/Logback_xcf.java
+++ src/main/java/com/soft/Logback/Logback_xcf.java
@@ -4,39 +4,63 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
 /**
  * @author 作者:大飞
- * @version 创建时间：2018年4月28日 上午10:28:11 类说明(测试用例)
+ * @version 创建时间：2018年4月28日 上午10:28:11 类说明
  */
 
-public class OwnLog extends AppenderBase<LoggingEvent> implements MessageListener {
-	// (测试用例)
+public class Logback_xcf extends AppenderBase<LoggingEvent> implements MessageListener {
+	public String providerURL;
+	private String Account;
+	private String Password; 
+	public String getProviderURL() {
+		return providerURL;
+	}
+	public String getAccount() {
+		return Account;
+	}
+	public void setAccount(String account) {
+		Account = account;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public void setProviderURL(String providerURL) {
+		this.providerURL = providerURL;
+	}
+
 	ConnectionFactory connectionFactory;
 	Connection connection;
 	Session session;
 	ThreadLocal<MessageProducer> threadLocal = new ThreadLocal<>();
 	private static Destination destination;
 
-	public OwnLog() throws Exception {
-		connectionFactory = new ActiveMQConnectionFactory("", "", "tcp://192.168.139.128:61616");
-		connection = connectionFactory.createConnection();
-		connection.start();
-		session = connection.createSession(true, Session.SESSION_TRANSACTED);
+	@Override
+	public void start() {
+		try {
+			started = true;//设置连接为true
+			connectionFactory = new ActiveMQConnectionFactory(Account, Password, providerURL);
+			connection = connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(true, Session.SESSION_TRANSACTED);
+		} catch (Exception e) {
+		}
 	}
 
 	@Override
 	protected void append(LoggingEvent eventObject) {
-		// LoggingEvent event = (LoggingEvent) ((ActiveMQObjectMessage)
-		// message).getObject();
 		try {
 			destination = session.createQueue("Jaycekon-MQ");
 			MessageProducer messageProducer = null;
@@ -58,7 +82,7 @@ public class OwnLog extends AppenderBase<LoggingEvent> implements MessageListene
 	}
 
 	@Override
-	public void onMessage(javax.jms.Message message) {
+	public void onMessage(Message message) {
 		// TODO Auto-generated method stub
 
 	}
