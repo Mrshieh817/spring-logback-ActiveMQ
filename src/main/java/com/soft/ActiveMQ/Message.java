f3f250d847db70cb373e69c7dee570c8dbe4cccf
Mrshieh817 <18716398693@163.com>
2018-07-12 11:00:55 +0800

    解释slf4j存在和屏蔽的区别，完善message的测试

--- src/main/java/com/soft/ActiveMQ/Message.java
+++ src/main/java/com/soft/ActiveMQ/Message.java
@@ -1,9 +1,9 @@
 package com.soft.ActiveMQ;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQObjectMessage;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggingEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import javax.jms.*;
 
@@ -13,10 +13,10 @@ import javax.jms.*;
  */
 
 public class Message implements MessageListener {
-
+	private Logger log = LoggerFactory.getLogger(Message.class);
 	public Message() throws Exception {
 		// (测试用例)
-		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.139.128:61616");
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin","admin","tcp://192.168.139.128:61616");
 		Connection connection = factory.createConnection();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		connection.start();
@@ -26,10 +26,10 @@ public class Message implements MessageListener {
 		consumer.setMessageListener(this);
 
 		// log a message
-		Logger logger = Logger.getLogger(Message.class);
-		logger.info("Info Log.");
-		logger.warn("Warn Log");
-		logger.error("Error Log.");
+	
+		log.debug("debug Log.");
+		log.warn("Warn Log");
+		log.error("Error Log.");
 
 		// clean up
 		Thread.sleep(1000);
@@ -39,14 +39,24 @@ public class Message implements MessageListener {
 		System.exit(1);
 	}
 
-	public void onMessage(javax.jms.Message message) {
-		try {
+	public void onMessage(javax.jms.Message msg) {
+		/*try {
 			// receive log event in your consumer
 			LoggingEvent event = (LoggingEvent) ((ActiveMQObjectMessage) message).getObject();
 			System.out.println("Received log [" + event.getLevel() + "]: " + event.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}*/
+		// TODO Auto-generated method stub
+		TextMessage message = (TextMessage)msg;
+		try {
+			log.info("receive message : "+ message.getText());
+			System.err.println("receive message : "+ message.getText());
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
 	}
 
 	public static void main(String[] args) throws Exception {
